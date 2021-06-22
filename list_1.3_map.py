9d1b5e00fdb259fe0c271010e1db4d4630420565
VladKli <vlad.klimovqqq@gmail.com>
2021-06-23 02:49:51 +0300

    done

--- list_1.3_map.py
+++ list_1.3_map.py
@@ -1,6 +1,15 @@
 # В масиві, який складається із n чисел, обчислити : Кількість елементів масиву, менших С (С – задається користувачем);
 # use map
 
+lis = [1, 2, 67, -3, 6, 0.25, -3.89, 4.52, -12, -7]
+
+nums = float(input('Set a digit: '))
+
+
+def calculate(c):
+    if c < nums:
+        return 1
+
 
 amount = list(map(calculate, lis))
 count = amount.count(1)
