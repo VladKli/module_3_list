3611280151489057ab4310a6dd8f307c4872d5f5
VladKli <vlad.klimovqqq@gmail.com>
2021-06-23 02:48:36 +0300

    done

--- list_1.3_comprehension.py
+++ list_1.3_comprehension.py
@@ -1,12 +1,10 @@
-# В масиві, який складається із n чисел, обчислити: Кількість елементів масиву, менших С (С – задається користувачем);
-# # use comprehension
+# В масиві, який складається із n чисел, обчислити : Кількість елементів масиву, менших С (С – задається користувачем);
+# use comprehension
+
+nums = float(input('Set a digit: '))
 
 lis = [1, 2, 67, -3, 6, 0.25, -3.89, 4.52, -12, -7]
-nums = float(input('Set a digit: '))
 
 amount = [num for num in lis if num < nums]
 
 print(len(amount))
-
-
-
